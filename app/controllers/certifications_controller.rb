e6a5cc1762d5d2d41250fd4d92a96df6a11095e4
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2023-04-26 08:19:42 -0600

    Created Certifications GET endpoint

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -1,10 +1,20 @@
 class CertificationsController < ApplicationController
+    def index
+        certifications = Certification.order('created_at DESC')
+        render json: {status: 'SUCCESS', message: 'Loaded certifications', data:certifications}, status: :ok
+    end
+
+    def new
+        @certification = Certification.new
+    end
+
     def create
         @certification = Certification.new(certification_params)
-    if @certification.save
-        render json: @certification, status: :created
-    else
-        render json: @certification.errors, status: :unprocessable_entity
+        if @certification.save
+            render json: @certification, status: :created
+        else
+            render json: @certification.errors, status: :unprocessable_entity
+        end
     end
 
     private
