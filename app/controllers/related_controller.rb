9b42b04fc6c2a982ebb4f27dd9971ffe9d59f9aa
Carlo <carlouxljlj@gmail.com>
2023-05-23 18:48:09 -0600

    Text similarity related3 endpoint

---
+++ app/controllers/related_controller.rb
@@ -0,0 +1,36 @@
+class RelatedController < ApplicationController
+    require 'string/similarity'
+
+    def related3
+        @certifications = Certification.all
+
+        values = []
+
+        @certification_name = Certification.where(id: params[:id]).select(:title)
+
+        if @certification_name.exists?
+            for i in @certifications
+                if (i.title != @certification_name.first.title) 
+                    similarity = String::Similarity.cosine i.title, @certification_name.first.title
+                    values.push({ similarity: similarity, id: i.id, title: i.title })
+                end
+            end
+        else
+            render json: {
+                status: 'FAILURE',
+                message: 'That certification ID does not exist'
+            }, status: :not_found
+            return
+        end
+
+        sorted_values = values.sort_by { |value| value[:similarity] }.reverse
+        highest_three_values = sorted_values.take(3)
+
+        render json: {
+            status: 'SUCCESS', 
+            message: '3 Most similar certifications', 
+            analyzed_title: @certification_name.first.title,
+            data: highest_three_values
+        }, status: :ok
+    end
+end
