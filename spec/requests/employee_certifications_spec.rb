e93c62ded1784f29564f2fd4a91a692a604216c5
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2023-05-30 20:53:37 -0600

    amountpermonth test & shared examples

--- spec/requests/employee_certifications_spec.rb
+++ spec/requests/employee_certifications_spec.rb
@@ -1,28 +1,33 @@
 require 'rails_helper'
 
+RSpec.shared_examples "a certification endpoint" do |endpoint|
+  let!(:certification) { FactoryBot.create(:certification) }
+
+  context 'Valid Certification id' do
+    it "returns data" do
+      10.times do
+        employee = FactoryBot.create(:employee)
+        FactoryBot.create(:employee_certification, employee: employee, certification: certification)
+      end
+
+      get "/#{endpoint}/#{certification.id}"
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to_not be_nil
+    end
+  end
+end
+
 RSpec.describe "EmployeeCertifications", type: :request do
   before do
     allow_any_instance_of(EmployeeCertificationsController).to receive(:authenticate_request)
   end
 
   describe "GET /latest5" do
-    let!(:certification) { FactoryBot.create(:certification) }
+    it_behaves_like "a certification endpoint", "latest5"
+  end
 
-    context 'Valid Certification id' do
-      it "Return data" do
-
-        10.times do
-          employee = FactoryBot.create(:employee)
-          FactoryBot.create(:employee_certification, employee: employee, certification: certification)
-        end
-
-        get "/latest5/#{certification.id}"
-
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to_not be_nil
-
-      end
-    end
-    
+  describe "GET amountPerMonth" do
+    it_behaves_like "a certification endpoint", "amntmonth"
   end
 end
