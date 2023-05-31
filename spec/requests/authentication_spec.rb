0209fe722822b19a1804f99fcd8d22d67b01df4e
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2023-05-30 21:03:53 -0600

    added shared examples to authentication controller

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
+RSpec.shared_examples "an invalid authentication" do |email, password|
+  it 'returns an unauthorized status' do
+    post :login, params: { email: email, password: password }
+
+    json_response = JSON.parse(response.body)
+    expect(response).to have_http_status(:unauthorized)
+    expect(json_response['token']).to eq('Invalid username or password')
+  end
+end
+
 RSpec.describe AuthenticationController, type: :controller do
   describe 'POST #login' do
     let!(:user) { FactoryBot.create(:user) }
 
     context 'with valid credentials' do
-      
       it 'returns a JWT token' do
         post :login, params: { email: 'usertest@ibm.com', password: '123456' }
 
@@ -16,23 +25,11 @@ RSpec.describe AuthenticationController, type: :controller do
     end
 
     context 'with invalid password' do
-      it 'returns an unauthorized status' do
-        post :login, params: { email: 'usertest@ibm.com', password: 'invalidpassword' }
-
-        json_response = JSON.parse(response.body)
-        expect(response).to have_http_status(:unauthorized)
-        expect(json_response['token']).to eq('Invalid username or password')
-      end
+      it_behaves_like "an invalid authentication", 'usertest@ibm.com', 'invalidpassword'
     end
 
     context 'with invalid email' do
-      it 'returns an unauthorized status' do
-        post :login, params: { email: 'invalidemail', password: '123456' }
-
-        json_response = JSON.parse(response.body)
-        expect(response).to have_http_status(:unauthorized)
-        expect(json_response['token']).to eq('Invalid username or password')
-      end
+      it_behaves_like "an invalid authentication", 'invalidemail', '123456'
     end
   end
 end
