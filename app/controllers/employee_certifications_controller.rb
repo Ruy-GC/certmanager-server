083a37ba379255be1ec8833a711a5185385627cc
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2023-06-06 20:11:39 -0600

    update employeecertification controller and tests

--- app/controllers/employee_certifications_controller.rb
+++ app/controllers/employee_certifications_controller.rb
@@ -1,75 +1,87 @@
 class EmployeeCertificationsController < ApplicationController
-    
+    before_action :find_certification, only: [:latest5, :amountPerMonth]
+  
     def new
-        @employee_certification = EmployeeCertification.new
+      @employee_certification = EmployeeCertification.new
     end
-
+  
     def create
-        @employee_certification = EmployeeCertification.new(employee_ertification_params)
-        if @employee_certification.save
-            render json: @employee_certification, status: :created
-        else
-            render json: @employee_certification.errors, status: :unprocessable_entity
-        end
+      @employee_certification = EmployeeCertification.new(employee_ertification_params)
+      if @employee_certification.save
+        render json: @employee_certification, status: :created
+      else
+        render json: @employee_certification.errors, status: :unprocessable_entity
+      end
     end
-
+  
     def latest5
-        @certifications = EmployeeCertification.where(certification_id: params[:id])
-            .select(:employee_id,:issued_date)
-            .order('issued_date DESC')
-            .first(5)
-            
-        render json: {
-            status: 'SUCCESS', 
-            message: '5 most recent certifications', 
-            data: @certifications
-            }, status: :ok
+      @certifications = EmployeeCertification.where(certification_id: params[:id])
+                                            .select(:employee_id, :issued_date)
+                                            .order('issued_date DESC')
+                                            .first(5)
+      render_certifications
     end
-
+  
     def empCert
-        @employee_certifications = EmployeeCertification.select('employee_certifications.employee_id,employee_certifications.certification_id, certifications.title, 
-        employee_certifications.issued_date, employees.work_location, certifications.skills, certifications.category')
-        .joins(:certification, :employee)
-
-        render json: {
-            status: 'SUCCESS', 
-            message: 'Employee Certifications', 
-            data: @employee_certifications
-            }, status: :ok
+      @certifications = EmployeeCertification.select('employee_certifications.employee_id,employee_certifications.certification_id, certifications.title, employee_certifications.issued_date, employees.work_location, certifications.skills, certifications.category')
+                                                     .joins(:certification, :employee)
+      render_certifications
     end
-
+  
     def amountPerMonth
-        ranges = {
-            '6 months' => (Date.today - 6.months)..Date.today,
-            '12 months' => (Date.today - 12.months)..(Date.today - 6.months),
-            '18 months' => (Date.today - 18.months)..(Date.today - 12.months),
-            '24 months' => (Date.today - 24.months)..(Date.today - 18.months)
+      ranges = {
+        '6 months' => (Date.today - 6.months)..Date.today,
+        '12 months' => (Date.today - 12.months)..(Date.today - 6.months),
+        '18 months' => (Date.today - 18.months)..(Date.today - 12.months),
+        '24 months' => (Date.today - 24.months)..(Date.today - 18.months)
+      }
+  
+      @counts = []
+  
+      ranges.each do |label, range|
+        obj = {
+          "name" => label,
+          "amt" => EmployeeCertification.where(certification_id: params[:id], issued_date: range).count
         }
-
-        @counts = []
-        
-        ranges.each do |label,range|
-            obj ={
-                "name" => label,
-                "amt" => EmployeeCertification.where(
-                    certification_id: params[:id],
-                    issued_date: range 
-                ).count
-            } 
-
-            @counts.push(obj)
-        end
-
-        render json: {
-            status: 'SUCCESS', 
-            message: 'Issued certifications', 
-            data: @counts.reverse()
-            }, status: :ok
+  
+        @counts.push(obj)
+      end
+  
+      render json: {
+        status: 'SUCCESS',
+        message: 'Issued certifications',
+        data: @counts.reverse()
+      }, status: :ok
     end
-
+  
     private
-
+  
     def employee_ertification_params
-        params.require(:certification).permit(:employee_id, :certification_id, :issued_date)
+      params.require(:certification).permit(:employee_id, :certification_id, :issued_date)
     end
-end
+  
+    def find_certification
+      unless Certification.exists?(id: params[:id])
+        render json: {
+          status: 'FAILURE',
+          message: 'That certification ID does not exist'
+        }, status: :not_found
+      end
+    end
+  
+    def render_certifications
+      if @certifications.present?
+        render json: {
+          status: 'SUCCESS',
+          message: 'Certifications',
+          data: @certifications
+        }, status: :ok
+      else
+        render json: {
+          status: 'FAILURE',
+          message: 'No certifications found'
+        }, status: :not_found
+      end
+    end
+  end
+  
--- spec/requests/employee_certifications_spec.rb
+++ spec/requests/employee_certifications_spec.rb
@@ -16,6 +16,16 @@ RSpec.shared_examples "a certification endpoint" do |endpoint|
       expect(response.body).to_not be_nil
     end
   end
+
+  context 'invalid id' do
+    it "gets invalid id" do
+      get "/#{endpoint}/invalidID"
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json_response['message']).to eq('That certification ID does not exist')
+    end
+  end
 end
 
 RSpec.describe "EmployeeCertifications", type: :request do
@@ -29,5 +39,22 @@ RSpec.describe "EmployeeCertifications", type: :request do
 
   describe "GET amountPerMonth" do
     it_behaves_like "a certification endpoint", "amntmonth"
+  end
+
+  describe "GET empcert" do
+    let!(:certification) { FactoryBot.create(:certification) }
+
+    it "returns data" do
+      
+      10.times do
+        employee = FactoryBot.create(:employee)
+        FactoryBot.create(:employee_certification, employee: employee, certification: certification)
+      end
+
+      get "/empCert"
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to_not be_nil
+    end
   end
 end
